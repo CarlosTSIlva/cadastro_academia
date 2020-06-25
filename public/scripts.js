0be3e1f00665b55e7e661b22add311f000674955
CarlosTeixeira615 <carlosoteixeira.tc@hotmail.com>
2020-06-25 00:54:09 -0300

    base do cadastro

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,9 @@
-const modalOverlay = document.querySelector('.modalover');
-const cards = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector('.modalover')
+const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
-	card.addEventListener('click', function() {
-		const videoId = card.getAttribute('id');
-		window.location.href = `/video?id=${videoId}`;
-	});
+  card.addEventListener('click', function () {
+    const videoId = card.getAttribute('id')
+    window.location.href = `/video?id=${videoId}`
+  })
 }
--- routes.js
+++ routes.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
 
-routes.get('/', function(req, res) {
-	return res.redirect('/instructors');
-});
+routes.get('/', function (req, res) {
+  return res.redirect('/instructors')
+})
 
-routes.get('/instructors', function(req, res) {
-	return res.render('instructors/index');
-});
-routes.get('/members', function(req, res) {
-	return res.send('members');
-});
-module.exports = routes;
+routes.get('/instructors', function (req, res) {
+  return res.render('instructors/index')
+})
+routes.get('/instructors/create', function (req, res) {
+  return res.render('instructors/create')
+})
+routes.post('/instructors', function (req, res) {
+  return res.send(req.body)
+})
+
+routes.get('/members', function (req, res) {
+  return res.render('members')
+})
+module.exports = routes
