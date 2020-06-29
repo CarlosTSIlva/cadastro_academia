0597a9b7d9a0a63a2ba461128a0e145f81365776
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-06-28 22:55:46 -0300

    delete.notfound

--- instructors.js
+++ instructors.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const data = require("./data.json");
 const { age, date } = require("./utils");
 const { resolveSoa } = require("dns");
+const { json } = require("express");
 
 exports.show = function (req, res) {
   const { id } = req.params;
@@ -103,3 +104,15 @@ exports.put = function (req, res) {
   });
 };
 //delete
+exports.delete = function (req, res) {
+  const { id } = req.body;
+  const filteredInstructors = data.instructors.filter(function (instructor) {
+    return instructor.id != id;
+  });
+
+  data.instructors = filteredInstructors;
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("write error");
+    return res.redirect("/instructors");
+  });
+};
