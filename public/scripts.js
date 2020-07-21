32aaf236f51e9c5908f61a145b32d7960dad4234
CarlosTeixeira615 <carlosteixeira.tc@hotmail.com>
2020-07-20 21:09:23 -0300

    final

--- public/scripts.js
+++ public/scripts.js
@@ -9,13 +9,13 @@ function paginate(selectedPage, totalPages) {
 
     if (
       firstAndLastPage ||
-      (pagesAfterSelectedPage && pagesBeforeSelectedPage)
+      pagesAfterSelectedPage && pagesBeforeSelectedPage
     ) {
       if (oldPage && currentPage - oldPage > 2) {
         pages.push("...");
       }
       if (oldPage && currentPage - oldPage == 2) {
-        pages.push(currentPage - 1);
+        pages.push(currentPage + 1);
       }
       pages.push(currentPage);
 
@@ -24,23 +24,37 @@ function paginate(selectedPage, totalPages) {
   }
   return pages;
 }
-const pagination = document.querySelector(".pagination")
-const filter= pagination.dataset.filter
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
-let elements = ""
 
-for(let page of pages){
-  if(String(page).includes("...")){
-    elements += `<span>${page}</span>`
-  } else {
-    if(filter){
-      elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
-    }else{
-      elements += `<a href="?page=${page}">${page}</a>`
+
+function createPagination(pagination) {
+
+  const filter = pagination.dataset.filter
+  const page = + pagination.dataset.page
+  const total = + pagination.dataset.total
+  const pages = paginate(page, total)
+
+  let elements = ""
+
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`
+    } else {
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+      }
     }
+
   }
+
+  pagination.innerHTML = elements
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+  createPagination(pagination)
+}
+
+
